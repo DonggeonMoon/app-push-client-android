6f5bccc7d3ac84dc85d56f101d82f16c8e525f03
Donggeon Moon <nullsafer@naver.com>
2024-08-04 21:41:35 +0900

    chore: android project setup

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mdg.cryptoconverter {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.mdg.cryptoconverter to javafx.fxml;
-    exports com.mdg.cryptoconverter;
-}
